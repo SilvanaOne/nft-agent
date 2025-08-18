5a8ffe5779ca6c9a6e1340a8f9e430f22a5adf78
DFST <mk@dfst.io>
2025-08-18 11:21:00 +0300

    0.4.9

--- test/agent.test.ts
+++ test/agent.test.ts
@@ -75,6 +75,8 @@ const {
 } = args;
 
 const DELAY = chain === "local" ? 1000 : chain === "zeko" ? 3000 : 10000;
+if (chain === "zeko:alphanet")
+  throw new Error("zeko:alphanet is not supported");
 
 const api = new NftAPI({
   jwt: useLocalCloudWorker ? "local" : JWT,
